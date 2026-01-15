41e508aad1fa44c0b9e2d9d445bd4d06ffe9c833
jmacjz <zielinskijohn626@gmail.com>
2026-01-14 21:09:44 -0500

    Basic Car Movement

---
+++ Racing Game/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CarMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField]
+    private float accelerationSpeed, maxSpeed;
+    [SerializeField]
+    private bool accelerating, braking;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (rb.linearVelocity.z > maxSpeed)
+        {
+            rb.linearVelocity = new Vector3(0, 0, maxSpeed);
+        }
+        else if (rb.linearVelocity.z <= 0)
+        { 
+            rb.linearVelocity = new Vector3(0, 0, 0);
+            braking = false;
+        }
+
+        Movement();
+    }
+
+    public void Movement()
+    {
+        if (accelerating)
+            rb.AddForce(new Vector3(0, 0, accelerationSpeed), ForceMode.Acceleration);
+
+        if (braking)
+        {
+            rb.AddForce(new Vector3(0, 0, -accelerationSpeed), ForceMode.Acceleration);
+        }
+    }
+
+    public void Drive(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            accelerating = true;
+        }
+
+        if (context.canceled)
+        {
+            accelerating = false;
+        }
+
+    }
+
+    public void Brake(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            braking = true;
+        }
+        if (context.canceled)
+        {
+            braking = false;
+        }
+
+    }
+}
